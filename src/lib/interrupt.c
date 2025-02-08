0380ef8e15aef284c87cca5aa6f29b8a4f09a9a8
Lucas Emanoel <lucaasifx@gmail.com>
2025-02-08 12:18:10 -0300

    :sparkles: feat: Implementação da rotina de alternância de estado do LED azul por interrupção

--- src/lib/interrupt.c
+++ src/lib/interrupt.c
@@ -22,8 +22,16 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
                 puts("\nLED verde desligado.");
             else
                 puts("\nLED verde ligado.");
-            // ALTERNA O ESTADO DO LED
+            // ALTERNA O ESTADO DO LED verde
             gpio_put(LED_GREEN, !gpio_get(LED_GREEN));
+        }
+        if(gpio == BUTTON_B) {
+            if(gpio_get(LED_BLUE))
+                puts("\nLED azul desligado.");
+            else
+                puts("\nLED azul ligado.");
+            // ALTERNA O ESTADO DO LED azul
+            gpio_put(LED_BLUE, !gpio_get(LED_BLUE));
         }
 
     }
